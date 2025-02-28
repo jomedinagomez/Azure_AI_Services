f30e92798d1015c1d4f7801bb8ae3e53ea0f17d4
jomedinagomez <jomedin@microsoft.com>
2025-02-27 21:03:56 -0500

    adding offline tiktoken mode

--- TestCSV/obj/Debug/net8.0/TestCSV.AssemblyInfo.cs
+++ TestCSV/obj/Debug/net8.0/TestCSV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestCSV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd264354f7054d4af2fb19c2ba27b078aeb9019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7c1aa35d8a5e6406a0b1f615c065baaa3625a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestCSV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestCSV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
